1e7f0dcaee6c54d7246b5f3e380f3fbc91a0eb18
DanielK0219 <dido.kolev0219@gmail.com>
2023-12-04 22:30:19 +0200

    Добавени Update и Delete Games ендпойнти

--- VGL_Project/Models/Interfaces/IGameService.cs
+++ VGL_Project/Models/Interfaces/IGameService.cs
@@ -4,6 +4,8 @@
     {
         public Task<bool> AddGame(string title, string gameDesc, string genre);
         public Task<Game?> GetGame(int id);
+        Task<bool> UpdateGame(int id, string newTitle, string newGameDesc, string newGenre);
+        Task<bool> DeleteGame(int id);
         public Task<IEnumerable<Game>?> GetGames();
     }
 }
