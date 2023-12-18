118b3713a92004ce5bd357d07bb5fc6131cee399
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2023-12-18 09:50:51 +0200

    implement get ownedGames by steam API

--- VGL_Project/Controllers/GamesController.cs
+++ VGL_Project/Controllers/GamesController.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using VGL_Project.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using SteamWebAPI2.Interfaces;
+using SteamWebAPI2.Utilities;
+using VGL_Project.Models.Constants;
 using VGL_Project.Models.Interfaces;
 
 namespace VGL_Project.Controllers
@@ -23,13 +23,40 @@ namespace VGL_Project.Controllers
 
             return Ok("Game Created");
         }
+        [HttpGet("get-all-owned-games-for-user")]
+        public async Task<IActionResult> GetAllGames(ulong steamID,bool includeAppInfo = false,bool includeFreeGames = false)
+        {
+            var webInterfaceFactory = new SteamWebInterfaceFactory(Constants.API_KEY);
+            var steamInterface = webInterfaceFactory.CreateSteamWebInterface<PlayerService>(new HttpClient());
+            var ownedGamesResponse = await steamInterface.GetOwnedGamesAsync(steamID, includeAppInfo, includeFreeGames);
+
+            if (ownedGamesResponse == null) return NotFound();
+
+            var data = ownedGamesResponse.Data;
+            return Ok(data);
+        }
+        [HttpGet("get-game-by-appId")]
+        public async Task<IActionResult> GetGameByID(uint appID)
+        {
+            var webInterfaceFactory = new SteamWebInterfaceFactory(Constants.API_KEY);
+            var steamInterface = webInterfaceFactory.CreateSteamWebInterface<SteamApps>(new HttpClient());
+            var ownedGamesResponse = await steamInterface.GetAppListAsync();
+
+            if (ownedGamesResponse == null) return NotFound();
+
+            var app = ownedGamesResponse.Data.FirstOrDefault(x => x.AppId == appID);
+
+            if (app == null) return NotFound();
+
+            return Ok(app);
+        }
 
         [HttpGet("get-game-by-id")]
         public async Task<IActionResult> GetGame(int id)
         {
             var result = await _gameService.GetGame(id);
 
-            if(result == null) return NotFound();
+            if (result == null) return NotFound();
 
             return Ok(result);
         }
