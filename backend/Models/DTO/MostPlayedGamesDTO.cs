649c8545e601f9778a871d599be5cb4d22c33a9b
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2024-01-07 01:35:01 +0200

    add mostplayeddto

---
+++ backend/Models/DTO/MostPlayedGamesDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VGL_Project.Models.DTO
+{
+    public class MostPlayedGamesDTO
+    {
+        [Required]
+        public required string SteamId { get; set; }
+        [Required]
+        public required int Count { get; set; }
+    }
+}
