10caeb5a9b51318c75d5d64e596bffcdef6ec7b4
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2024-01-07 01:34:52 +0200

    add newsdto

---
+++ backend/Models/DTO/NewsDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VGL_Project.Models.DTO
+{
+    public class NewsDTO
+    {
+        [Required]
+        public required string SteamId { get; set; }
+
+        [Required]
+        public required int MaxCount { get; set; }
+    }
+}
