4f9804ea6110e4237bf62ef13385a2de5e42851f
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2024-01-07 19:15:21 +0200

    add newsEndpointResponse

--- backend/Models/DTO/NewsDTO.cs
+++ backend/Models/DTO/NewsDTO.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace VGL_Project.Models.DTO
 {
@@ -9,5 +11,35 @@ namespace VGL_Project.Models.DTO
 
         [Required]
         public required int MaxCount { get; set; }
+    }
+    public class NewsEndpointResponse
+    {
+        public string AppId { get; set; }
+        public string GameName { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("author")]
+        public string Author { get; set; }
+
+        private string _contents;
+
+        [JsonProperty("contents")]
+        public string Contents
+        {
+            get => _contents;
+            set => _contents = RemoveNonTextContent(value);
+        }
+
+        private static string RemoveNonTextContent(string input)
+        {
+            // Remove HTML tags, images, and other non-text content
+            string pattern = @"\{[^}]*\}|\[(\/?[^\]]+)\]|\[img\]|\[\/img\]|<[^>]+>|https?://\S+|\n";
+            string cleanedInput = Regex.Replace(input, pattern, string.Empty);
+
+            return cleanedInput.Length <= 500 ? cleanedInput : cleanedInput[..500];
+        }
+
     }
 }
