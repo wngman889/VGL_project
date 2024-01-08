5cf0927cf8ed976d68e4b2f77bb10f5c08256712
Petko Todorov <petkoptodorov@gmail.com>
2024-01-08 21:18:18 +0200

    Add more unit tests for reviews

--- VGL_Project.Tests/Controller/ReviewsControllerTests.cs
+++ VGL_Project.Tests/Controller/ReviewsControllerTests.cs
@@ -1,10 +1,16 @@
 ﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VGL_Project.Controllers;
+using VGL_Project.Data;
+using VGL_Project.Models;
 using VGL_Project.Models.Interfaces;
 using VGL_Project.Services;
 
@@ -18,5 +24,55 @@ namespace VGL_Project.Tests.Controller
         {
             _reviewService = A.Fake<IReviewService>();
         }
+
+        [Fact]
+        public async Task GetReviews_ReturnsOkResultWithReviews()
+        {
+            // Arrange
+            var reviews = A.CollectionOfFake<ReviewRecommendation>(3);
+            var controller = new ReviewsController(_reviewService);
+
+            // Act
+            var result = await controller.GetReviews();
+
+            // Assert
+            result.Should().NotBeNull().And.BeOfType<OkObjectResult>();
+            var okResult = result as OkObjectResult;
+            okResult.Value.Should().BeAssignableTo<IEnumerable<ReviewRecommendation>>();
+        }
+
+        [Fact]
+        public async Task GetReviews_ReturnsEmptyList()
+        {
+            // Arrange
+            var emptyReviews = new List<ReviewRecommendation>(); // An empty list
+            A.CallTo(() => _reviewService.GetReviews()).Returns(emptyReviews);
+            var controller = new ReviewsController(_reviewService);
+
+            // Act
+            var result = await controller.GetReviews();
+
+            // Assert
+            result.Should().NotBeNull().And.BeOfType<OkObjectResult>();
+            var okResult = result as OkObjectResult;
+            var reivewList = okResult.Value as IEnumerable<ReviewRecommendation>;
+            reivewList.Should().NotBeNull().And.BeEmpty();
+        }
+
+        [Fact]
+        public async Task GetReviews_ReturnsInternalServerErrorResultOnException()
+        {
+            // Arrange
+            var dbContext = A.Fake<VGLDbContext>();
+            A.CallTo(() => dbContext.ReviewRecommendations).Throws<Exception>();
+            var controller = new ReviewsController(_reviewService);
+
+            // Act
+            var result = await controller.GetReviews();
+
+            // Assert
+            result.Should().BeOfType<StatusCodeResult>().Which.StatusCode.Should().Be(500);
+        }
+
     }
 }
