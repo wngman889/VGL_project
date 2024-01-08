839046512bf0e311b91c22d81e449161313ee307
Petko Todorov <petkoptodorov@gmail.com>
2024-01-08 21:28:20 +0200

    Add UserControllerTests class

---
+++ VGL_Project.Tests/Controller/UserControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VGL_Project.Models.Interfaces;
+using VGL_Project.Services;
+
+namespace VGL_Project.Tests.Controller
+{
+    public class UserControllerTests
+    {
+        private readonly IUserService _userService;
+
+        public UserControllerTests()
+        {
+            _userService = A.Fake<IUserService>();
+        }
+    }
+}
