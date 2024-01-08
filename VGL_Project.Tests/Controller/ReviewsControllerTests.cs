0d95257f55bddeee11f6163ae5c623a5eb72a97e
Petko Todorov <petkoptodorov@gmail.com>
2024-01-08 19:03:56 +0200

    New review class for tests

---
+++ VGL_Project.Tests/Controller/ReviewsControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VGL_Project.Models.Interfaces;
+using VGL_Project.Services;
+
+namespace VGL_Project.Tests.Controller
+{
+    public class ReviewsControllerTests
+    {
+        private readonly IReviewService _reviewService;
+
+        public ReviewsControllerTests()
+        {
+            _reviewService = A.Fake<IReviewService>();
+        }
+    }
+}
